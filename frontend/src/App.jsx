5d920681d8186a101ab4b34c49d58fed9180106c
om sai mishara <omsaimishra95568@gmail.com>
2025-10-15 18:25:44 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 // Use environment variable (works locally and in Docker)
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3901";
+const API_URL = import.meta.env.VITE_API_URL=http://13.233.173.243:3901;
 
 function App() {
   const [formData, setFormData] = useState({
