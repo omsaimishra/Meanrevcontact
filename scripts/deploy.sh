0f220a70f685d54276258390282803f2e94c8b38
om sai mishara <omsaimishra95568@gmail.com>
2025-10-15 17:06:54 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,12 @@ cd /home/ubuntu/contact
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 143262365667.dkr.ecr.ap-south-1.amazonaws.com
 
 # Stop and remove old containers
-docker-compose down || true
+docker compose down || true
 
 # Pull the latest images
-docker-compose pull
+docker compose pull
 
 # Start new containers
-docker-compose up -d
+docker compose up -d
 
 echo " Deployment completed successfully!"
